51109a790983f1e20425fd9769cf08337283e4ad
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-12-03 03:02:22 +0300

    practise on .fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
   
-}
 
+const inputForm = document.querySelector('form');
+
+inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+   // display the value of the input in the console
+
+   const input = document.querySelector('input#searchByID');
+
+   console.log(input.value);
+   //using fetch request
+   fetch(`http://localhost:3000/movies/${input.value}`)
+   .then(response => response.json())
+   .then(data => {
+    //replace title(h4) and summary(p)
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+
+    //assigning new values to h4 and p
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+
+    // console.log(data);
+
+   });
+ 
+    
+});
+}
 document.addEventListener('DOMContentLoaded', init);
